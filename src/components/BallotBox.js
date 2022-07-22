787f344a9044af8110ca4c384fa61dc2025fcd07
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-07-22 06:32:14 +0200

    feat: Vote metrics
    * Show vote count & percentage share on Q when Q is answered.

--- src/components/BallotBox.js
+++ src/components/BallotBox.js
@@ -1,24 +1,31 @@
-
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 // import { handleAnswerQuestion } from "../actions/questions";
 import { handleAnswerQuestion } from "../actions/shared";
+import getVoteMetrics from "../utils/PollMetrics";
 
 /*
-* 1. Radio buttons for selecting? -> One button for submit?.
-* 2. 2 text views
-* 3. 2 options
-* 4. 2 indicators for votes cast.
-*
-*  */
+ * 1. Radio buttons for selecting? -> One button for submit?.
+ * 2. 2 text views
+ * 3. 2 options
+ * 4. 2 indicators for votes cast.
+ *
+ *  */
 
 
 const BallotBox = (props) => {
 
-	const { authedUser } = useSelector(state => state);
+	const { authedUser, questions } = useSelector(state => state);
 	const dispatch = useDispatch();
+	const pollQuestion = props.question;
+	const [pollMetrics, setPollMetrics] = useState(getVoteMetrics(pollQuestion));
+
+	useEffect(() => {
+		setPollMetrics(getVoteMetrics(pollQuestion));
+	}, [pollMetrics])
 
 	const handleVoteClick = (e) => {
 
@@ -31,39 +38,40 @@ const BallotBox = (props) => {
 	}
 
 	return (
-		<div className="ballotBox">
-			<div className="ballot">
-				<div className="ballotText">
-					<div className="voteQuestionText">
-						<Typography variant="body2" color="text.secondary">
-							{ props.question.optionOne.text }
+		<div className = "ballotBox">
+			<div className = "ballot">
+				<div className = "ballotText">
+					<div className = "voteQuestionText">
+						<Typography variant = "body2" color = "text.secondary">
+							{props.question.optionOne.text}
 						</Typography>
 					</div>
-					<div className="voteShareMetrics">
-						<Typography variant="body2" color="text.secondary">
-							81% (4)
-						</Typography>
-					</div>
+					{((props.question.optionOne.votes.includes(authedUser)) || (props.question.optionOne.votes.includes(authedUser))) &&
+						(<div className = "voteShareMetrics">
+							<Typography variant = "body2" color = "text.secondary">
+								{`${pollMetrics.optionOnePercentage}% (${pollMetrics.optionOneVoteCount})`}
+							</Typography>
+						</div>)}
 				</div>
-				<Button size="small" onClick={handleVoteClick} id="optionOne" value="optionOne">
+				<Button size = "small" onClick = {handleVoteClick} id = "optionOne" value = "optionOne">
 					Vote
 				</Button>
 			</div>
 
-			<div className="ballot">
-				<div className="ballotText">
-					<div className="voteQuestionText">
-						<Typography variant="body2" color="text.secondary">
-							{ props.question.optionTwo.text }
+			<div className = "ballot">
+				<div className = "ballotText">
+					<div className = "voteQuestionText">
+						<Typography variant = "body2" color = "text.secondary">
+							{props.question.optionTwo.text}
 						</Typography>
 					</div>
-					<div className="voteShareMetrics">
-						<Typography variant="body2" color="text.secondary">
+					<div className = "voteShareMetrics">
+						<Typography variant = "body2" color = "text.secondary">
 							81% (4)
 						</Typography>
 					</div>
 				</div>
-				<Button size="small" >
+				<Button size = "small">
 					Vote
 				</Button>
 			</div>

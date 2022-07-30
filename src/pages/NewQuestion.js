bf32f4b0443b0061ce2a2dd3fc2fee5a8579cf36
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-07-30 21:53:38 +0200

    test: fireEvent test
    * Error message shown when user misses adding a choice option

--- src/pages/NewQuestion.js
+++ src/pages/NewQuestion.js
@@ -1,7 +1,7 @@
-import { FormControl, TextField } from "@mui/material";
+import { Alert, FormControl, Snackbar, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { handleAddQuestion } from "../actions/questions";
 
 
@@ -12,7 +12,7 @@ const NewQuestion = () => {
 	const dispatch = useDispatch();
 	const [optionOneText, setOptionOneText] = useState("");
 	const [optionTwoText, setOptionTwoText] = useState("");
-	const [newPoll, setNewPoll] = useState(false);
+	const [pollError, setPollError] = useState(false);
 
 	/* Local state variables */
 	const [authenticated, setAuthenticated] = useState(false);
@@ -43,15 +43,27 @@ const NewQuestion = () => {
 
 		// TODO: Add Question to store.
 
-		console.log("optionOneText: ", optionOneText);
-		console.log("optionTwoText: ", optionTwoText);
+		if (optionOneText !== "" && optionTwoText !== "") {
+			console.log("optionOneText: ", optionOneText);
+			console.log("optionTwoText: ", optionTwoText);
 
-		dispatch(handleAddQuestion(optionOneText, optionTwoText, authedUser));
+			dispatch(handleAddQuestion(optionOneText, optionTwoText, authedUser));
 
-		/* Reset form values and redirect user to the questions page. */
-		setOptionOneText("");
-		setOptionTwoText("");
-		navigate('/home');
+			/* Reset form values and redirect user to the questions page. */
+			setOptionOneText("");
+			setOptionTwoText("");
+			navigate('/home');
+		} else {
+			setPollError(true);
+		}
+	};
+
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+
+		setPollError(false);
 	};
 
 	const optionOneCharactersLeft = 100 - optionOneText.length;
@@ -77,11 +89,16 @@ const NewQuestion = () => {
 						<div className = "option-text-length">{optionTwoCharactersLeft}</div>}
 				</FormControl>
 
-				<button className = "btn " type = "submit" disabled = {optionOneText === "" || optionTwoText === ""}
-						size = "medium" color = "secondary">
+				<button className = "btn " type = "submit" size = "medium" color = "secondary"
+						data-testid="submit-question">
 					Submit
 				</button>
 			</form>
+			<Snackbar open = {pollError} autoHideDuration = {6000} onClose = {handleClose} data-testid="poll-error-message">
+				<Alert onClose = {handleClose} severity = "error" sx = {{ width: '100%' }} >
+					Please provide both choice options for the question!
+				</Alert>
+			</Snackbar>
 		</div>
 	);
 };
